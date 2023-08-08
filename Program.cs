0f2ce93f32f5597aff3eea96f62899e880b76939
Chengming <v-cruan@microsoft.com>
2023-08-08 10:34:33 +0800

    update

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using Microsoft.Azure.Management.Fluent;
-using Microsoft.Azure.Management.ResourceManager.Fluent;
-using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
-using Microsoft.Azure.Management.ResourceManager.Fluent.Models;
-using Microsoft.Azure.Management.Samples.Common;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Threading;
+using Azure.ResourceManager.Samples.Common;
+using Azure.Identity;
+using Azure.ResourceManager;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.ResourceManager.Resources;
+using Azure;
+using Azure.ResourceManager.Resources.Models;
 
 namespace DeployUsingARMTemplateWithProgress
 {
@@ -18,59 +21,61 @@ namespace DeployUsingARMTemplateWithProgress
          * Azure Resource sample for deploying resources using an ARM template and
          * showing progress.
          */
-        public static void RunSample(IAzure azure)
+        public static async Task RunSample(ArmClient client)
         {
-            string rgName = SdkContext.RandomResourceName("rgRSAP", 24);
-            string deploymentName = SdkContext.RandomResourceName("dpRSAP", 24);
+            // Get default subscription
+            SubscriptionResource subscription = await client.GetDefaultSubscriptionAsync();
+
+            // Create a resource group in the EastUS region
+            string rgName = Utilities.CreateRandomName("ARMTemplateRG");
+            Utilities.Log($"created resource group with name:{rgName}");
+            ArmOperation<ResourceGroupResource> rgLro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, new ResourceGroupData(AzureLocation.EastUS));
+            ResourceGroupResource resourceGroup = rgLro.Value;
+            Utilities.Log("Created a resource group with name: " + resourceGroup.Data.Name);
 
             try
             {
-                var templateJson = Utilities.GetArmTemplate("ArmTemplate.json");
-
                 //=============================================================
-                // Create resource group.
+                // Create a deployment for an Azure App Service via an ARM template.
 
-                Utilities.Log("Creating a resource group with name: " + rgName);
+                string applicationDefinitionName = Utilities.CreateRandomName("sampleApplicationDefinition");
 
-                azure.ResourceGroups.Define(rgName)
-                        .WithRegion(Region.USWest)
-                        .Create();
+                Utilities.Log("Starting a deployment for an Azure App Service: " + applicationDefinitionName);
 
-                Utilities.Log("Created a resource group with name: " + rgName);
-
-                //=============================================================
-                // Create a deployment for an Azure App Service via an ARM
-                // template.
-
-                Utilities.Log("Starting a deployment for an Azure App Service: " + deploymentName);
-
-                azure.Deployments.Define(deploymentName)
-                        .WithExistingResourceGroup(rgName)
-                        .WithTemplate(templateJson)
-                        .WithParameters("{}")
-                        .WithMode(DeploymentMode.Incremental)
-                        .BeginCreate();
-
-                Utilities.Log("Started a deployment for an Azure App Service: " + deploymentName);
-
-                var deployment = azure.Deployments.GetByResourceGroup(rgName, deploymentName);
-                Utilities.Log("Current deployment status : " + deployment.ProvisioningState);
-
-                while (!(StringComparer.OrdinalIgnoreCase.Equals(deployment.ProvisioningState, "Succeeded") || 
-                        StringComparer.OrdinalIgnoreCase.Equals(deployment.ProvisioningState, "Failed") || 
-                        StringComparer.OrdinalIgnoreCase.Equals(deployment.ProvisioningState, "Cancelled")))
+                // First we need to get the application definition collection from the resource group
+                ArmApplicationDefinitionCollection applicationDefinitionCollection = resourceGroup.GetArmApplicationDefinitions();
+                // Use the same location as the resource group
+                var input = new ArmApplicationDefinitionData(resourceGroup.Data.Location, ArmApplicationLockLevel.None)
                 {
-                    SdkContext.DelayProvider.Delay(10000);
-                    deployment = azure.Deployments.GetByResourceGroup(rgName, deploymentName);
-                    Utilities.Log("Current deployment status : " + deployment.ProvisioningState);
-                }
+                    DisplayName = applicationDefinitionName,
+                    Description = $"{applicationDefinitionName} description",
+                    PackageFileUri = new Uri("https://raw.githubusercontent.com/Azure/azure-managedapp-samples/master/Managed%20Application%20Sample%20Packages/201-managed-storage-account/managedstorage.zip")
+                };
+                ArmOperation<ArmApplicationDefinitionResource> lro = await applicationDefinitionCollection.CreateOrUpdateAsync(WaitUntil.Completed, applicationDefinitionName, input);
+                ArmApplicationDefinitionResource applicationDefinition = lro.Value;
+
+                Utilities.Log("Started a deployment for an Azure App Service: " + applicationDefinitionName);
+
+                //Utilities.Log(applicationDefinition.Data);
+
+                //var deployment = azure.Deployments.GetByResourceGroup(rgName, deploymentName);
+                //Utilities.Log("Current deployment status : " + deployment.ProvisioningState);
+
+                //while (!(StringComparer.OrdinalIgnoreCase.Equals(deployment.ProvisioningState, "Succeeded") ||
+                //        StringComparer.OrdinalIgnoreCase.Equals(deployment.ProvisioningState, "Failed") ||
+                //        StringComparer.OrdinalIgnoreCase.Equals(deployment.ProvisioningState, "Cancelled")))
+                //{
+                //    SdkContext.DelayProvider.Delay(10000);
+                //    deployment = azure.Deployments.GetByResourceGroup(rgName, deploymentName);
+                //    Utilities.Log("Current deployment status : " + deployment.ProvisioningState);
+                //}
             }
             finally
             {
                 try
                 {
                     Utilities.Log("Deleting Resource Group: " + rgName);
-                    azure.ResourceGroups.DeleteByName(rgName);
+                    await resourceGroup.DeleteAsync(WaitUntil.Completed);
                     Utilities.Log("Deleted Resource Group: " + rgName);
                 }
                 catch (NullReferenceException)
@@ -84,21 +89,20 @@ namespace DeployUsingARMTemplateWithProgress
             }
         }
 
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             try
             {
                 //=================================================================
                 // Authenticate
-                var credentials = SdkContext.AzureCredentialsFactory.FromFile(Environment.GetEnvironmentVariable("AZURE_AUTH_LOCATION"));
+                var clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+                var clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
+                var tenantId = Environment.GetEnvironmentVariable("TENANT_ID");
+                var subscription = Environment.GetEnvironmentVariable("SUBSCRIPTION_ID");
+                ClientSecretCredential credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+                ArmClient client = new ArmClient(credential, subscription);
 
-                var azure = Azure
-                    .Configure()
-                    .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
-                    .Authenticate(credentials)
-                    .WithDefaultSubscription();
-
-                RunSample(azure);
+                await RunSample(client);
             }
             catch (Exception ex)
             {
